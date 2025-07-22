43c6ba9b68b440c05c8524dfab33480dea17eb45
Prateek Aghi <prateek@samanalytic.com>
2025-07-23 04:05:25 +0530

    empty resules component added

--- Frontend/src/components/Cards/SpendingDetails.jsx
+++ Frontend/src/components/Cards/SpendingDetails.jsx
@@ -14,9 +14,11 @@ import {
   ShoppingCart,
   DirectionsCar,
   Work,
+  SentimentDissatisfiedOutlined,
 } from "@mui/icons-material";
 import { ResponsiveContainer, PieChart, Pie, Cell, Legend } from "recharts";
 import { useUserTransactionCategorySummary } from "../../hooks/useTransactions";
+import EmptyResults from "./EmptyResults";
 
 const currentMonthStats = {
   expenses: 3456.78,
@@ -32,143 +34,113 @@ const chartColors = [
   "#6366f1",
 ];
 
-const categories = [
-  {
-    name: "Food & Dining",
-    amount: 1234.56,
-    icon: Restaurant,
-    color: "bg-red-500",
-    chartColor: "#ef4444",
-  },
-  {
-    name: "Transportation",
-    amount: 567.89,
-    icon: DirectionsCar,
-    color: "bg-blue-500",
-    chartColor: "#3b82f6",
-  },
-  {
-    name: "Shopping",
-    amount: 890.12,
-    icon: ShoppingCart,
-    color: "bg-green-500",
-    chartColor: "#22c55e",
-  },
-  {
-    name: "Entertainment",
-    amount: 345.67,
-    icon: SportsEsports,
-    color: "bg-purple-500",
-    chartColor: "#a855f7",
-  },
-  {
-    name: "Bills & Utilities",
-    amount: 234.56,
-    icon: Home,
-    color: "bg-orange-500",
-    chartColor: "#f97316",
-  },
-  {
-    name: "Business",
-    amount: 123.45,
-    icon: Work,
-    color: "bg-indigo-500",
-    chartColor: "#6366f1",
-  },
-];
-
 const SpendingDetails = () => {
-  const { data, isLoading, isError, error } =
+  const { data, isLoading, isError, error, refetch } =
     useUserTransactionCategorySummary();
 
   if (isLoading) return <p>Loading.....</p>;
   if (isError) return <p>{error}</p>;
 
-  console.log(data.data);
-
   return (
-    <Card sx={{ borderRadius: "0.5rem", border: "1px solid #e4e4e7" }}>
-      <CardHeader
-        title={
-          <Typography variant="h6" fontWeight="medium">
-            Spending by Category
-          </Typography>
-        }
-        subheader={
-          <Typography variant="body2" color="text.secondary">
-            Your top spending categories this month
-          </Typography>
-        }
-      />
+    <>
+      {data.data.length > 0 ? (
+        <Card sx={{ borderRadius: "0.5rem", border: "1px solid #e4e4e7" }}>
+          <CardHeader
+            title={
+              <Typography variant="h6" fontWeight="medium">
+                Spending by Category
+              </Typography>
+            }
+            subheader={
+              <Typography variant="body2" color="text.secondary">
+                Your top spending categories this month
+              </Typography>
+            }
+          />
 
-      <CardContent>
-        <Box height={250}>
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie
-                data={data.data}
-                cx="50%"
-                cy="50%"
-                innerRadius={60}
-                outerRadius={80}
-                paddingAngle={2}
-                dataKey="amount"
-                nameKey="category"
-                isAnimationActive={false}
-              >
-                {data.data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={chartColors[index]} />
-                ))}
-              </Pie>
-              <Legend
-                layout="horizontal"
-                verticalAlign="bottom"
-                align="center"
-                formatter={(value) => <span className="text-xs">{value}</span>}
-              />
-            </PieChart>
-          </ResponsiveContainer>
-        </Box>
-
-        {data.data.map((category, index) => {
-          return (
-            <Box
-              key={index}
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              mt={2}
-            >
-              <Box display="flex" alignItems="center" gap={2}>
-                <Avatar
-                  sx={{
-                    bgcolor: category.color,
-                    width: 32,
-                    height: 32,
-                  }}
-                  variant="rounded"
-                >
-                  {/* <Icon style={{ fontSize: 16, color: "#fff" }} /> */}
-                </Avatar>
-
-                <Box>
-                  <Typography fontWeight={500}>{category.category}</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {category.percentage} % of {category.total}
-                  </Typography>
-                </Box>
-              </Box>
-
-              <Box textAlign="right">
-                <Typography fontWeight={500}>
-                  $ {category.amount.toLocaleString()}
-                </Typography>
-              </Box>
+          <CardContent>
+            <Box height={250}>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={data.data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={80}
+                    paddingAngle={2}
+                    dataKey="amount"
+                    nameKey="category"
+                    isAnimationActive={false}
+                  >
+                    {data.data.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={chartColors[index]} />
+                    ))}
+                  </Pie>
+                  <Legend
+                    layout="horizontal"
+                    verticalAlign="bottom"
+                    align="center"
+                    formatter={(value) => (
+                      <span className="text-xs">{value}</span>
+                    )}
+                  />
+                </PieChart>
+              </ResponsiveContainer>
             </Box>
-          );
-        })}
-      </CardContent>
-    </Card>
+
+            {data.data.map((category, index) => {
+              return (
+                <Box
+                  key={index}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  mt={2}
+                >
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <Avatar
+                      sx={{
+                        bgcolor: category.color,
+                        width: 32,
+                        height: 32,
+                      }}
+                      variant="rounded"
+                    >
+                      {/* <Icon style={{ fontSize: 16, color: "#fff" }} /> */}
+                    </Avatar>
+
+                    <Box>
+                      <Typography fontWeight={500}>
+                        {category.category}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {category.percentage} % of {category.total}
+                      </Typography>
+                    </Box>
+                  </Box>
+
+                  <Box textAlign="right">
+                    <Typography fontWeight={500}>
+                      $ {category.amount.toLocaleString()}
+                    </Typography>
+                  </Box>
+                </Box>
+              );
+            })}
+          </CardContent>
+        </Card>
+      ) : (
+        <EmptyResults
+          title="Nothing to see here"
+          description="Add Transaction to see category based expenses."
+          IconComponent={SentimentDissatisfiedOutlined}
+          showAction={true}
+          onAction={() => refetch()}
+          actionLabel="Reload"
+        />
+      )}
+    </>
   );
 };
 
