716c2c13157957b4b564916c6d965185620ee0c5
Prateek Aghi <prateek@samanalytic.com>
2025-07-24 01:50:14 +0530

    Updating store values on profile update

--- Frontend/src/Containers/Profile/ProfileForm.jsx
+++ Frontend/src/Containers/Profile/ProfileForm.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import GenericForm from "../../components/Forms/GenericForm";
+import PageHeader from "../../components/navigation/PageHeader";
 import { useUpdateUser } from "../../hooks/useUsers";
+import { useAuthStore } from "../../store/authStore";
 
 const ProfileForm = () => {
+  const auth = useAuthStore.getState();
+  const [initialState, setInitialState] = useState({
+    profile_image: auth.profile_image || null,
+    first_name: auth.first_name || "",
+    last_name: auth.last_name || "",
+  });
+
   const { mutateAsync, isPending } = useUpdateUser();
 
   if (isPending) return <p>Pending...</p>;
@@ -11,7 +21,6 @@ const ProfileForm = () => {
       const response = await mutateAsync({
         first_name: data.first_name,
         last_name: data.last_name,
-        timezone: data.timezone,
         profile_image: data.profile_image,
       });
     } catch (error) {
@@ -20,29 +29,25 @@ const ProfileForm = () => {
   };
 
   return (
-    <GenericForm
-      initialState={{
-        profile_image: null,
-        first_name: "",
-        last_name: "",
-        timezone: "",
-      }}
-      validationRules={{
-        first_name: { required: true },
-        last_name: { required: true },
-        timezone: { required: false },
-        profile_image: { required: false },
-      }}
-      fieldConfigs={{
-        first_name: { label: "First Name" },
-        last_name: { label: "Last Name" },
-        timezone: { label: "Timezone" },
-        profile_image: { label: "Profile Picture", type: "file" },
-      }}
-      onSubmit={submitHandler}
-      submitLabel="Save"
-      redirectUrl="/profile"
-    />
+    <>
+      <PageHeader backTo={"/dashboard"} title={"User Profile"} />
+      <GenericForm
+        initialState={initialState}
+        validationRules={{
+          first_name: { required: true },
+          last_name: { required: true },
+          profile_image: { required: false },
+        }}
+        fieldConfigs={{
+          first_name: { label: "First Name" },
+          last_name: { label: "Last Name" },
+          profile_image: { label: "Profile Picture", type: "file" },
+        }}
+        onSubmit={submitHandler}
+        submitLabel="Save"
+        redirectUrl="/dashboard"
+      />
+    </>
   );
 };
 
